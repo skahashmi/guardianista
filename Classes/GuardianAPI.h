830f9781766a72318da140ca1746a09201cf3c99
Matt Biddulph <matt@hackdiary.com>
2009-03-04 11:24:11 +0000

    3rd-party libs

---
+++ Classes/GuardianAPI.h
@@ -0,0 +1,16 @@
+//
+//  GuardianAPI.h
+//  Guardian
+//
+//  Created by Matt Biddulph on 04/03/2009.
+//  Copyright 2009 Hackdiary Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GuardianAPI : NSObject {
+
+}
+
+@end
---
+++ Classes/GuardianAPI.m
@@ -0,0 +1,14 @@
+//
+//  GuardianAPI.m
+//  Guardian
+//
+//  Created by Matt Biddulph on 04/03/2009.
+//  Copyright 2009 Hackdiary Ltd. All rights reserved.
+//
+
+#import "GuardianAPI.h"
+
+
+@implementation GuardianAPI
+
+@end
