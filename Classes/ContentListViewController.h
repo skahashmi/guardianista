916f6d3f0d9e8749fb2d9aa9cae3016cabd83ad8
Matt Biddulph <matt@hackdiary.com>
2009-03-05 04:26:00 -0800

    ContentListViewController and a top-level menu

---
+++ Classes/ContentListViewController.h
@@ -0,0 +1,19 @@
+//
+//  ContentListViewController.h
+//  Guardian
+//
+//  Created by Matt Biddulph on 05/03/2009.
+//  Copyright 2009 Hackdiary Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ContentListViewController : UITableViewController {
+	NSArray *contents;
+	IBOutlet UITableView *tableView;
+}
+
+@property (nonatomic, retain) UITableView *tableView;
+@property (nonatomic, retain) NSArray *contents;
+@end
---
+++ Classes/ContentListViewController.m
@@ -0,0 +1,167 @@
+//
+//  ContentListViewController.m
+//  Guardian
+//
+//  Created by Matt Biddulph on 05/03/2009.
+//  Copyright 2009 Hackdiary Ltd. All rights reserved.
+//
+
+#import "GuardianAppDelegate.h"
+#import "GuardianContent.h"
+#import "ContentListViewController.h"
+#import "ContentViewController.h"
+
+
+@implementation ContentListViewController
+@synthesize contents;
+@synthesize tableView;
+
+- (void)setContentList:(NSArray *)content {
+	NSLog(@"Content:\n%@", content);
+	self.contents = content;
+	[[self tableView] reloadData];
+}
+
+/*
+- (id)initWithStyle:(UITableViewStyle)style {
+    // Override initWithStyle: if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+    if (self = [super initWithStyle:style]) {
+    }
+    return self;
+}
+*/
+
+/*
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+*/
+
+- (void)viewWillAppear:(BOOL)animated {
+	GuardianAppDelegate *delegate = (GuardianAppDelegate *)[[UIApplication sharedApplication] delegate];
+
+    [super viewWillAppear:animated];
+	self.contents = [NSArray array];
+	[delegate.guardian latestContentWithDelegate:self didSucceedSelector:@selector(setContentList:)];
+}
+
+/*
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+*/
+/*
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+}
+*/
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
+}
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return [self.contents count];
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    GuardianContent *content = [self.contents objectAtIndex:indexPath.row];
+	cell.text = content.headline;
+
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	GuardianContent *content = [self.contents objectAtIndex:indexPath.row];
+	ContentViewController *contentViewController;
+	contentViewController = [[ContentViewController alloc] 
+							 initWithNibName:@"ContentViewController"
+							 bundle:nil]; 
+	contentViewController.url = content.url;
+	[self.navigationController pushViewController:contentViewController animated:YES]; 
+	[contentViewController release]; 
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
+
